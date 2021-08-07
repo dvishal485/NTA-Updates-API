73969af6516a27f2cc7d948b8b47e0c89e4f5433
Vishal <dvishal485@gmail.com>
2021-08-07 20:46:08 +0530

    redefined header

--- header.js
+++ header.js
@@ -1,4 +1,4 @@
-const header = (req_headers) => {
+const header = () => {
     return {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "*",
@@ -6,9 +6,6 @@ const header = (req_headers) => {
         "Access-Control-Max-Age": "86400",
         "Content-Type": "application/json",
         "Cache-Control": "no-cache, no-store, must-revalidate",
-        "Your-IP": req_headers.get("cf-connecting-ip"),
-        "Your-Country": req_headers.get("CF-IPCountry"),
-        "Host": req_headers.get("host"),
         "Made-By": atob('VmlzaGFsIERhcyBodHRwczovL2dpdGh1Yi5jb20vZHZpc2hhbDQ4NQ==')
     }
 }
