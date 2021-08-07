7542e1d999e3ace59fd8a08c5f9e05a816d265a7
Vishal <dvishal485@gmail.com>
2021-08-07 20:46:25 +0530

    added error handling

--- index.js
+++ index.js
@@ -2,32 +2,33 @@ import getUpdates from './getUpdates'
 import header from './header'
 
 async function handleRequest(request) {
-    const headers = header(request.headers)
+    const headers = header()
     const path = new URL(request.url).pathname;
 
     if (request.method == 'GET') {
         if (path.startsWith('/events')) {
-            return new Response(await getUpdates('events'), {
-                status: 200,
-                headers
-            })
+            return await getUpdates('events')
         } else if (path.startsWith('/news')) {
-            return new Response(await getUpdates('news'), {
-                status: 200,
-                headers
-            })
+            return await getUpdates('news')
         } else if (path.startsWith('/status')) {
-            return new Response(await getUpdates('status'), {
-                status: 200,
-                headers
-            })
+            return await getUpdates('status')
+        } else if (path.startsWith('/special')) {
+            return await getUpdates('special')
         } else {
+            const url = `https://jeemain.nta.nic.in/webinfo2021`
+            const alive = (await fetch(url)).ok
             return new Response(JSON.stringify({
-                alive: true,
-                repository_name: 'JEE-Updates-API',
+                website_alive: alive,
+                repository_name: 'JEE Updates API',
                 repository_description: 'JEE Updates API written in javascript to fetch latest data from JEE Mains website',
                 repository_url: 'https://github.com/dvishal485/JEE-Updates-API',
                 made_by: 'https://github.com/dvishal485',
+                api_endpoints: {
+                    'events': 'https://jee-updates-api.dvishal485.workers.dev/events',
+                    'news': 'https://jee-updates-api.dvishal485.workers.dev/news',
+                    'status': 'https://jee-updates-api.dvishal485.workers.dev/status',
+                    'special': 'https://jee-updates-api.dvishal485.workers.dev/special'
+                }
             }, null, 2), {
                 status: 200,
                 headers
